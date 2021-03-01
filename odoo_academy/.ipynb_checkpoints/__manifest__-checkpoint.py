7f0d001805397d5e2f7e4002baa06045e0fbe284
Victor <vi.fperez@gmail.com>
2021-03-01 13:28:28 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Odoo Academy",
+    
+    "summary": """Academy app to manage Training""",
+    
+    "description": """
+        Academy module to manage training:
+            -Courses
+            -Sessions
+            -Attendees
+    """,
+    
+    "author": "vfperez",
+    
+    "website": "https://www.ampsoftware.com",
+    
+    "category": "training",
+    "version": "0.1",
+    
+    "depends": ["base"],
+    
+    "data": [
+        
+    ],
+    
+    "demo": [
+        
+    ],
+}
