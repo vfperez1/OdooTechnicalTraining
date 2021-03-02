37a2f716cf642559b552ee45fad545bc50f6e0da
Victor <vi.fperez@gmail.com>
2021-03-02 16:41:18 +0000

    [FIX]models/course

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -21,7 +21,7 @@ class Course(models.Model):
     
     base_price = fields.Float(string="Base Price", default=0.00)
     
-    additional_fee = fields.Float(string="Additional Fee", default=0.00)
+    additional_fee = fields.Float(string="Additional Fee", default=10.00)
     
     total_price = fields.Float(string="Total Price", readonly=True)
     
