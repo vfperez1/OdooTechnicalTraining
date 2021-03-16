47eac45efc0a571f6080e14bd0527e1187e051ae
Victor <vi.fperez@gmail.com>
2021-03-16 13:12:36 +0000

    [ADD]Basic Module

---
+++ SpaceMission/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Starship",
+    
+    "summary": """Academy app to manage Training""",
+    
+    "description": """
+        Academy Module to manage Training:
+         -Courses
+         -Sessions
+         -Attendees
+    """,
+    
+    "author": "vfperez",
+    
+    "website": "https://ampsoftware.com",
+    
+    "category": "Training",
+    "version": "0.1",
+    
+    "depends": ["base"],
+    
+    "data": [
+        
+    ],
+    
+    "demo": [
+        
+    ],
+    
+}
