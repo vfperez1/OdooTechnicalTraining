c70fdf03c35ece70a46e05f413a9c6a733ea5933
Victor <vi.fperez@gmail.com>
2021-04-22 10:09:27 +0000

    [ADD]TwoFields_RESPARTNER

--- SpaceMission/models/.ipynb_checkpoints/contactos_amp-checkpoint.py
+++ SpaceMission/models/.ipynb_checkpoints/contactos_amp-checkpoint.py
@@ -5,10 +5,11 @@ from odoo import models, fields, api
 class ContactosAmp(models.Model):
     _inherit = "res.partner"
     
-    conceptos = fields.Char(string="Conceptos")
-    concepto_1 = fields.Char(string="Concepto 1")
-    concepto_2 = fields.Char(string="Concepto 2")
-    concepto_2 = fields.Char(string="Concepto 3")
-    mantenimiento_d3 = fields.Boolean(string="Mantenimiento D3")
-    actividad = fields.Integer(string="Activdad")
+    x_conceptos = fields.Char(string="Conceptos")
+    x_concepto_1 = fields.Text()
+    x_concepto_2 = fields.Char(string="Concepto 2")
+    x_concepto_2 = fields.Char(string="Concepto 3")
+    x_mantenimiento_d3 = fields.Boolean(string="Mantenimiento D3")
+    x_actividad = fields.Integer(string="Activdad")
+    x_telefono_2 = fields.Char()
     
